e17bf2d1376bd460a08fccdbc60e2f9ece02c5bb
Simon Lee <simonlsc@yahoo.com>
2021-05-29 01:04:28 +0000

    modify vpc module 1

--- vpc.tf
+++ vpc.tf
@@ -1,16 +1,16 @@
 module "vpc" {
     source  = "terraform-google-modules/network/google//modules/vpc"
-    version = "~> 3.2.2"
+    version = "~> 2.0.0"
 
     project_id   = var.project
     network_name = "gaurav-network"
 
     subnets = [
-    {
-        subnet_name   = "gaurav-subnet"
-        subnet_ip     = "10.100.10.0/24"
-        subnet_region = var.region
-    }
+        {
+            subnet_name   = "gaurav-subnet"
+            subnet_ip     = "10.100.10.0/24"
+            subnet_region = var.region
+        }
     ]
-    shared_vpc_host = false
+    
 }
