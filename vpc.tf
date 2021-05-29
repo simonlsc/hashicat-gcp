1d590e604bd5fb3ab48420418921c99e5298b976
Simon Lee <simonlsc@yahoo.com>
2021-05-29 01:06:36 +0000

    modify vpc module 3.2.2

--- vpc.tf
+++ vpc.tf
@@ -1,6 +1,6 @@
 module "vpc" {
     source  = "terraform-google-modules/network/google//modules/vpc"
-    version = "~> 2.0.0"
+    version = "~> 3.2.2"
 
     project_id   = var.project
     network_name = "gaurav-network"
