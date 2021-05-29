d663df335d10ddef1331d689195cfb221366e547
Simon Lee <simonlsc@yahoo.com>
2021-05-29 01:11:16 +0000

    modify vpc module 4

--- vpc.tf
+++ vpc.tf
@@ -5,12 +5,10 @@ module "vpc" {
     project_id   = var.project
     network_name = "gaurav-network"
 
-    subnets = [
-        {
-            subnet_name   = "gaurav-subnet"
-            subnet_ip     = "10.100.10.0/24"
-            subnet_region = var.region
-        }
-    ]
+    subnets = {
+        subnet_name   = "gaurav-subnet"
+        subnet_ip     = "10.100.10.0/24"
+        subnet_region = var.region
+    }
     
 }
