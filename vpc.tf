0bc64359986f278d01cd1dd193def634f84c813d
Simon Lee <simonlsc@yahoo.com>
2021-05-29 01:28:12 +0000

    modify vpc module add back

---
+++ vpc.tf
@@ -0,0 +1,17 @@
+module "vpc" {
+    source  = "terraform-google-modules/network/google//modules/subnets"
+    version = "~> 3.2.2"
+
+    project_id   = var.project
+    network_name = "gaurav-network"
+
+    subnets = [
+  {
+    subnet_name   = "gaurav-subnet"
+    subnet_ip     = "10.100.10.0/24"
+    subnet_region = var.region
+  }
+]
+
+}
+
