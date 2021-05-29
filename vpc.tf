0089a27920303ff3b3e8f083ca018a3ab26973e3
Simon Lee <simonlsc@yahoo.com>
2021-05-29 01:36:34 +0000

    modify vpc module abc

--- vpc.tf
+++ vpc.tf
@@ -3,11 +3,11 @@ module "vpc" {
     version = "~> 3.2.2"
 
     project_id   = var.project
-    network_name = "gaurav-network"
+    network_name = "abc-network"
 
     subnets = [
   {
-    subnet_name   = "gaurav-subnet"
+    subnet_name   = "abc-subnet"
     subnet_ip     = "10.100.10.0/24"
     subnet_region = var.region
   }
